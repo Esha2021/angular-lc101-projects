be5ee13393c1cf6fa2255b49b8ff2fa81a014454
Esha2021 <eeswaricse@gmail.com>
2021-08-12 22:06:00 -0500

    lesson3angular

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+​
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,9 +7,77 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Exercises: Angular Lesson 3';
-
+​
   color = 'green';
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  takeOffEnabled: boolean = true;
+​
+  gutterCheck(width, height) {
+    if (width > 260000 || width < 0 || height < 0 || height > 340000) {
+      this.color = 'orange';
+    } else {
+      this.color = 'blue';
+    }
+  }
+​
+  moveRocket(rocketImage, direction) {
+    if (direction === 'right') {
+      const movement = parseInt(rocketImage.style.left) + 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width = this.width + 10000;
+    } else if (direction === 'left') {
+      const movement = parseInt(rocketImage.style.left) - 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width= this.width - 10000;
+    } else if (direction === 'down') {
+      let movement = parseInt(rocketImage.style.bottom) - 10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.height = this.height - 10000;
+    } else {
+      let movement = parseInt(rocketImage.style.bottom) + 10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.height = this.height + 10000;
+    }
+    this.gutterCheck(this.width, this.height);
+  }
+​
+  handleTakeOff() {
+    let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    if (result) {
+       this.color = 'blue';
+       this.height = 10000;
+       this.width = 0;
+       this.message = 'Shuttle in flight.';
+       this.takeOffEnabled = false;
+    }
+ }
+​
+ handleLand(rocketImage) {
+  let result = window.confirm('Are you sure you want to return Earth? ARE YOU SURE?!');
+  if (result) {
+     this.color = 'green';
+     this.height = 0;
+     this.width = 0;
+     this.message = 'Shuttle has landed';
+     rocketImage.style.bottom = '0px';
+     this.takeOffEnabled = true;
+  }
+ }
+  handleMissionAbort(rocketImage) {
+    let result = window.confirm('Are you sure you want to abort the mission?');
+    if (result) {
+       this.color = 'red';
+       this.height = 0;
+       this.width = 0;
+       this.message = 'Mission aborted.';
+       rocketImage.style.bottom = '0px';
+       this.takeOffEnabled = true;
+​
+    }
+ }
+​
 }
+
+
